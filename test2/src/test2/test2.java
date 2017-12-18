049083a1e411091fa23c2b6622596ce8c2093b95
Loren1573 <13865513261@163.com>
2017-12-18 08:46:27 +0800

    rename

--- test2/src/test2/test2.java
+++ test2/src/test2/test2.java
@@ -1,9 +1,14 @@
 package test2;
 import static net.mindview.util.Print.*;
+import java.lang.Math;
+import java.util.*;
 
 public class test2 {
 	
-    static class Dog{  //由于主方法是static的，内部类是动态的；类中静态方法不能直接调用动态方法
+    class Dog{  //由于主方法是static的，内部类是动态的；类中静态方法不能直接调用动态方法
+    			//解决方案 ：
+    			//			1：把内部类改为静态 static eg：statc class Dog
+    			//          2：先创建外部类实例再创建内部类实例 例子如下
     	String names;
     	String says;
     }
@@ -20,12 +25,19 @@ public class test2 {
     	
     	/*        */
     	
-    	Dog spot = new Dog();
-    	Dog scruffy = new Dog();
-    	spot.names = "spot";
-    	spot.says = "Ruff";
-    	scruffy.names = "scruffy";
-    	scruffy.says = "Wurf";
+    	Dog dog1 = new test2().new Dog();
+    	Dog dog2 = new test2().new Dog();
+    	dog1.names = "spot";
+    	dog1.says = "Ruff";
+    	dog2.names = "scruffy";
+    	dog2.says = "Wurf";
+    	
+    	print("dog1 name is " + dog1.names + " it says " + dog1.says);
+    	print("dog2 name is " + dog2.names + " it says " + dog2.says);
+    	
+    	/*     */
+    	print(Math.E);
+    	Random random = new Random(47);
         
     }
     
